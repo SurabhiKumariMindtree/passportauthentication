dce9f59195593b8de0541875c18495f525e0e303
SurabhiKumariMindtree <surabhi.surabhi.sahay@gmail.com>
2017-04-12 09:00:15 +0530

    Add files via upload

---
+++ passportauthentication/passport/easy-node-authentication/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'webtechdevops.centralindia.cloudapp.azure.com:51003/limspassport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
